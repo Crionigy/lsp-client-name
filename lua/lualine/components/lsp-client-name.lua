cad47548e043d290f7891ce6b6e253638c349b50
Crionigy <souki.arthur@gmail.com>
2025-01-21 20:22:09 -0300

    Create lsp-client-name.lua
    In name of god may be this the first and last commit

---
+++ lua/lualine/components/lsp-client-name.lua
@@ -0,0 +1,41 @@
+local M = require("lualine.component"):extend()
+
+M.init = function(self, options)
+  M.super.init(self, options)
+end
+
+M.lsp_client_name = function()
+  local sign = "★"
+  local result = sign
+  local client_names = {}
+  local clients = vim.lsp.get_clients()
+  local buffer_filetype = vim.api.nvim_buf_get_option(0, "filetype")
+
+  if next(clients) == nil then
+    return result
+  end
+
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+
+    if filetypes and vim.fn.index(filetypes, buffer_filetype) ~= -1 then
+      local name = client.name
+
+      table.insert(client_names, name)
+    end
+  end
+
+  if #client_names > 0 then
+    table.sort(client_names, function(a, b) return #a < #b end)
+    result = table.concat(client_names, " | ")
+  end
+
+  return result
+end
+
+
+M.update_status = function(self)
+  return self.lsp_client_name()
+end
+
+return M
