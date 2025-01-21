64e13e07470ab13475e07313156b8440ee4b67e3
Arthur Hassan Souki <souki.arthur@gmail.com>
2025-01-21 20:45:42 -0300

    Update lsp-client-name.lua

--- lua/lualine/components/lsp-client-name.lua
+++ lua/lualine/components/lsp-client-name.lua
@@ -1,5 +1,15 @@
 local M = require("lualine.component"):extend()
 
+M.replacements = {
+  [1] = { pattern = "_language_server$", replacement = "" },
+  [2] = { pattern = "tsserver$", replacement = "typescript" },
+  [3] = { pattern = "-tools$", replacement = "" },
+  [4] = { pattern = "_ls$", replacement = "" },
+  [5] = { pattern = "ls$", replacement = "" },
+  [6] = { pattern = "_+$", replacement = "" },
+  [7] = { pattern = "-+$", replacement = "" },
+}
+
 M.init = function(self, options)
   M.super.init(self, options)
 end
@@ -21,6 +31,10 @@ M.lsp_client_name = function()
     if filetypes and vim.fn.index(filetypes, buffer_filetype) ~= -1 then
       local name = client.name
 
+      for _, entry in ipairs(M.replacements) do
+        name = name:gsub(entry.pattern, entry.replacement)
+      end
+      
       table.insert(client_names, name)
     end
   end
